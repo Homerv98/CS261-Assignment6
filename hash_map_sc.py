46b23fb0f0a787165d7dcc176b506feea5facd50
Homero <vazquezho@oregonstate.edu>
2026-03-12 11:30:04 -0500

    Version 1.1

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -209,7 +209,7 @@ class HashMap:
 
 def find_mode(da: DynamicArray) -> tuple[DynamicArray, int]:
     """
-    TODO: Write this implementation
+    TODO: Write this implementatio
     """
     # if you'd like to use a hash map,
     # use this instance of your Separate Chaining HashMap
